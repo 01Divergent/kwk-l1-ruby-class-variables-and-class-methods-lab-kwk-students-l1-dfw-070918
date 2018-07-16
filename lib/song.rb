ce8903eb3c88e697af7ca080061f66ec23c6cef3
01Divergent <iphone.claudia@icloud.com>
2018-07-16 14:29:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,4 +4,4 @@ class Song
   Song.artists # => ["Jay-Z", "Drake", "Beyonce"]
   Song.genres # => ["Rap", "Pop"]
   Song.genre_count {"rap" => 5, "rock" => 1, "country" => 3}
-  Song.artist_count # => {"Beyonce" 
+  Song.artist_count # => {"Beyonce" => 17, "Jay-Z" => 40}
